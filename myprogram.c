f06b5369f153d55bd15f0231dc4e77f18201d035
repl.it user <replituser@example.com>
2021-02-20 11:36:12 +0000

    program compiled, comments added

--- myprogram.c
+++ myprogram.c
@@ -6,7 +6,7 @@ int factorial(int n)
 
 	//TODO: factorial of n (n! = 1*2*...*n)
   for(int i=1; i<=n; i++){
-    result *= i;
+    result *= i; // update the result for every increment
   }
 	return result;
 }
@@ -17,8 +17,8 @@ int sum_of_odd_numbers(int *array, int count)
 	for (int i=0; i<count; ++i)
 	{
 		//TODO: only add odd numbers, e.g., 3. Skip even numbers, e.g., 4
-    if(array[i]%2 != 0){
-		  sum += array[i];
+    if(array[i]%2 != 0){ // check if the current number is odd
+		  sum += array[i]; // if odd, add to the sum
     }
 	}
 	return sum;
@@ -28,9 +28,9 @@ int min_of_numbers(int *array, int count)
 {
 	//TODO: return the minimum number from the array
   int min = array[0];
-  for(int i=0; i<count; i++){
-    if(array[i] < min){
-      min = array[i];
+  for(int i=0; i<count; i++){ // check every element in the array
+    if(array[i] < min){ // check if current number less than previous 
+      min = array[i]; // assign it to min
     }
   }
 	return min;
@@ -42,9 +42,9 @@ int reversed_number(int number)
 	
 	int reversed = 0;
   while(number != 0){
-    reversed *= 10;
-    reversed += number%10;
-    number /= 10;
+    reversed *= 10; // this operation is for digit slicing
+    reversed += number%10; // update the reversed number with the last digit of given number
+    number /= 10; // remove last digit of given number to check next digit 
   }
 
 	return reversed;
@@ -55,8 +55,8 @@ char alphabet_index(int index)
 {
 	//TODO: for index 0, return 'a'. index 1, 'b', etc. until 25 for 'z'.
 	//if index is out of range, return space ' '.
-  char letters[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-  if(index <= 25)
+  char letters[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}; // initialize the letters in an array
+  if(index <= 25) // check index
 	  return letters[index];
   return ' ';
 }
