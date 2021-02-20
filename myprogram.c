ae09e6cf2cf1c18f6ed5c59613935d288605f882
repl.it user <replituser@example.com>
2021-02-20 11:02:50 +0000

    function 4 completed

--- myprogram.c
+++ myprogram.c
@@ -41,6 +41,11 @@ int reversed_number(int number)
 	//TODO: if input is 12345, return 54321
 	
 	int reversed = 0;
+  while(number != 0){
+    reversed *= 10;
+    reversed += number%10;
+    number /= 10;
+  }
 
 	return reversed;
 }
